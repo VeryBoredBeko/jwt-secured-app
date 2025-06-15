b3015cbc661f5da123703277c56e424f43ccb780
Beknur Tumenov <veryboredbeko@gmail.com>
2025-06-15 16:33:27 +0600

    Set refresh-token HTTPOnly cookie

--- src/main/java/org/boreebeko/jwt_secured_application/web/controller/auth/AuthController.java
+++ src/main/java/org/boreebeko/jwt_secured_application/web/controller/auth/AuthController.java
@@ -1,12 +1,15 @@
 package org.boreebeko.jwt_secured_application.web.controller.auth;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.boreebeko.jwt_secured_application.exception.AuthenticationException;
 import org.boreebeko.jwt_secured_application.service.auth.AuthenticationService;
 import org.boreebeko.jwt_secured_application.web.dto.auth.SignInRequest;
 import org.boreebeko.jwt_secured_application.web.dto.auth.SignUpRequest;
 import org.boreebeko.jwt_secured_application.web.dto.auth.jwt.JWTAuthenticationResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,9 +34,21 @@ public class AuthController {
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<JWTAuthenticationResponse> refresh(@RequestBody JWTAuthenticationResponse jwtAuthenticationResponse) {
+    public ResponseEntity<Void> refresh(HttpServletResponse response,
+                                                             @RequestBody JWTAuthenticationResponse jwtAuthenticationResponse) {
         try {
-            return new ResponseEntity<>(authenticationService.refresh(jwtAuthenticationResponse.getRefreshToken()), HttpStatus.OK);
+
+            JWTAuthenticationResponse authenticationResponse =
+                    authenticationService.refresh(jwtAuthenticationResponse.getRefreshToken());
+
+            ResponseCookie cookie = ResponseCookie
+                    .from("refresh-token", authenticationResponse.getRefreshToken())
+                    .httpOnly(true)
+                    .build();
+
+            response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+            return new ResponseEntity<>(HttpStatus.OK);
         } catch (AuthenticationException exception) {
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
         }
